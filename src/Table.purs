20e23f273aa10193140c8a330992f3df66407017
Philosopher <NasalMusician@gmail.com>
2017-02-26 15:46:12 -0600

    Prune imports, rename operators
    Operators match what would be corresponding bits of HTML.

--- src/Table.purs
+++ src/Table.purs
@@ -12,13 +12,9 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import MDL as MDL
 import MDL.Button as Button
-import MDL.Card as Card
 import MDL.Layout as Layout
-import MDL.Shadow as Shadow
 import MDL.Textfield as Textfield
-import CSS (marginLeft, marginRight, rad, vGradient)
-import CSS.Common (auto)
-import Control.Bind ((=<<))
+import CSS.TextAlign (leftTextAlign, textAlign)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
 import DOM (DOM)
@@ -26,13 +22,17 @@ import DOM.Event.Types (Event, MouseEvent)
 import DOM.HTML.Indexed.InputType (InputType(..))
 import DOM.HTML.Types (HTMLInputElement)
 import Data.Maybe (Maybe(..))
-import Data.Vector (Vec(..), (!!), (+>), empty, toArray, mapWithIndex)
+import Data.Vector (Vec, empty, toArray, (+>))
 import Halogen.HTML.CSS (style)
 import Unsafe.Coerce (unsafeCoerce)
 
 single :: forall a b. (Array (HH.HTML a b) -> HH.HTML a b) -> HH.HTML a b -> HH.HTML a b
 single node child = node [ child ]
-infixr 9 single as <\>
+infixr 9 single as ><
+
+oftext :: forall a b. (Array (HH.HTML a b) -> HH.HTML a b) -> String -> HH.HTML a b
+oftext node = single node <<< HH.text
+infixr 8 oftext as />
 
 data Query a
     = ToggleState a
@@ -108,31 +108,16 @@ mdiv classes children =
 mdiv1 :: forall a b. HH.ClassName -> Array (HH.HTML a b) -> HH.HTML a b
 mdiv1 = mdiv <<< Array.singleton
 
-card :: forall a b. Array (HH.HTML a b) -> HH.HTML a b
-card =
-    HH.div
-        [ HP.classes [ MDL.card, Shadow._2Dp ]
-        , style do
-            marginLeft auto
-            marginRight auto
-        ]
-
-thtext :: forall a b. String -> HH.HTML a b
-thtext = HH.th_ <<< Array.singleton <<< HH.text
-
-tdtext :: forall a b. String -> HH.HTML a b
-tdtext = HH.td_ <<< Array.singleton <<< HH.text
-
 table :: forall cell nRows nCols a b. (Nat nRows, Nat nCols) => Inflection nRows nCols cell -> (cell -> HH.HTML a b) -> HH.HTML a b
-table datable mapper = HH.table_ <\> HH.tbody_ (map HH.tr_ (Array.cons header rows))
+table datable mapper = HH.table_><HH.tbody_ (map HH.tr_ (Array.cons header rows))
     where
-      header = Array.cons (thtext "") $ map thtext $ toArray datable.colLabels
+      header = Array.cons (HH.th_/> "") $ map (HH.th[style (textAlign leftTextAlign)]/> _) $ toArray datable.colLabels
       labels = toArray datable.rowLabels
       label i = Maybe.fromMaybe "" $ Array.index labels i
       rows =
         Array.mapWithIndex rowOf $ toArray datable.cells
       rowOf i cells =
-        Array.cons (thtext $ label i) (map mapper $ toArray cells)
+        Array.cons (HH.th_/> label i) (map mapper $ toArray cells)
 
 ui :: forall eff. H.Component HH.HTML Query Unit Void (Aff (dom :: DOM | eff))
 ui = H.component { render, eval, initialState: const initialState, receiver: const Nothing }
@@ -140,10 +125,10 @@ ui = H.component { render, eval, initialState: const initialState, receiver: con
 
   render :: State -> H.ComponentHTML Query
   render state =
-    mdiv1 Layout.container <\>
-      mdiv [ MDL.layout ] <\>
-        mdiv1 Layout.content <\>
-            table nomen tdtext
+    mdiv1 Layout.container
+    ><mdiv [ MDL.layout ]
+      ><mdiv1 Layout.content
+        ><table nomen (input (HE.input UserInput) [] "Form of nomen" >>> (HH.td_><_))
 
   eval :: Query ~> H.ComponentDSL State Query Void (Aff (dom :: DOM | eff))
   eval (ToggleState next) = do
