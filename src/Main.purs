be49ad2dc5cd75459413a7aed7d34b4ecd74b8d7
Nicholas Scheel <nasalmusician@gmail.com>
2017-03-16 21:46:53 -0500

    Start getting verb table working
    Types subject to change. Definitely needs to be reorganized at some point.

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,7 @@ import Halogen.Aff (HalogenEffects)
 import Halogen.Aff.Util (awaitBody, runHalogenAff)
 import Halogen.VDom.Driver (runUI)
 
-import Example (ui)
+import Table (ui)
 
 main :: Eff (HalogenEffects ()) Unit
 main = runHalogenAff do
--- src/Table.purs
+++ src/Table.purs
@@ -1,7 +1,7 @@
 module Table where
 
 import Prelude
-import Data.Typelevel.Num
+import UIHelpers
 import DOM.Event.Event as Event
 import DOM.HTML.HTMLInputElement as HInput
 import Data.Array as Array
@@ -11,28 +11,22 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import MDL as MDL
-import MDL.Button as Button
 import MDL.Layout as Layout
-import MDL.Textfield as Textfield
 import CSS.TextAlign (leftTextAlign, textAlign)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
 import DOM (DOM)
 import DOM.Event.Types (Event, MouseEvent)
-import DOM.HTML.Indexed.InputType (InputType(..))
 import DOM.HTML.Types (HTMLInputElement)
-import Data.Maybe (Maybe(..))
+import Data.Foldable (maximum)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
+import Data.Typelevel.Num (class Nat, D2, D5)
 import Data.Vector (Vec, empty, toArray, (+>))
+import Debug.Trace (spy)
+import Global.Unsafe (unsafeStringify)
 import Halogen.HTML.CSS (style)
 import Unsafe.Coerce (unsafeCoerce)
-
-single :: forall a b. (Array (HH.HTML a b) -> HH.HTML a b) -> HH.HTML a b -> HH.HTML a b
-single node child = node [ child ]
-infixr 9 single as ><
-
-oftext :: forall a b. (Array (HH.HTML a b) -> HH.HTML a b) -> String -> HH.HTML a b
-oftext node = single node <<< HH.text
-infixr 8 oftext as />
 
 data Query a
     = ToggleState a
@@ -47,6 +41,186 @@ type Inflection nRows nCols cell =
 type Declension cell = Inflection D5 D2 cell
 type Noun = Declension String
 
+class Show d <= Display d where
+    display :: forall a b. d -> HH.HTML a b
+
+instance unitLabel :: Display Unit where
+    display _ = HH.text ""
+
+instance emptyLabel :: Display Void where
+    display _ = HH.text ""
+
+instance identityDisplay :: Display String where
+    display = HH.text
+
+newtype TableSection sectionT majRT minRT majCT minCT dataT =
+    TableSection
+        { section :: sectionT
+        , rows :: Headers majRT minRT
+        , cols :: Headers majCT minCT
+        , getCell :: sectionT -> majRT -> minRT -> majCT -> minCT -> dataT
+        }
+
+
+instance showTableSection :: (
+    Show sectionT, Show majRT, Show minRT, Show majCT, Show minCT, Show dataT
+) => Show (TableSection sectionT majRT minRT majCT minCT dataT) where
+    show (TableSection { section, rows, cols, getCell }) =
+        unsafeStringify
+            { section: show section
+            , rows: map showHeader rows
+            , cols: map showHeader cols
+            }
+        where
+            showHeader :: forall a b. (Show a, Show b) => Header a b -> String
+            showHeader { label, sub: [] } =
+                show label
+            showHeader { label, sub } =
+                show label <> " " <> show sub
+
+{-
+instance showTableSection :: Show (TableSection Unit Degree minRT majCT minCT dataT) where
+    show (TableSection { section, rows, cols, getCell }) =
+        show $ map showHeader rows
+        where
+            showHeader { label } = show label
+-}
+
+newtype CompoundTable sectionT majRT minRT majCT minCT dataT
+    = CompoundTable (Array (TableSection sectionT majRT minRT majCT minCT dataT))
+
+
+instance showTable :: (
+    Show sectionT, Show majRT, Show minRT, Show majCT, Show minCT, Show dataT
+) => Show (CompoundTable sectionT majRT minRT majCT minCT dataT) where
+    show (CompoundTable t) = show t
+
+
+--instance showTable :: Show (CompoundTable Unit Degree minRT majCT minCT dataT) where show (CompoundTable t) = show t
+
+simpleTable :: forall majRT minRT majCT minCT dataT.
+    { rows :: Headers majRT minRT
+    , cols :: Headers majCT minCT
+    , getCell :: majRT -> minRT -> majCT -> minCT -> dataT
+    } -> CompoundTable Unit majRT minRT majCT minCT dataT
+simpleTable { rows, cols, getCell } =
+    { section: unit
+    , rows: rows
+    , cols: cols
+    , getCell: const getCell
+    } # TableSection # Array.singleton # CompoundTable
+
+simpleVertical :: forall majRT minRT majCT minCT dataT.
+    { rows :: Array majRT
+    , getCell :: majRT -> dataT
+    } -> CompoundTable Unit majRT minRT majCT minCT dataT
+simpleVertical { rows, getCell } =
+    { section: unit
+    , rows: fullRows
+    , cols: []
+    , getCell: \_ row _ _ _ -> getCell row
+    } # TableSection # Array.singleton # CompoundTable
+    where
+        fullRows =
+            map (\label -> { label, sub: [] }) rows
+
+type Header majT minT =
+    { label :: majT
+    , sub :: Array minT
+    }
+type Headers majT minT = Array (Header majT minT)
+
+
+computeGutterWidth :: forall sectionT majRT minRT majCT minCT dataT
+     . CompoundTable sectionT majRT minRT majCT minCT dataT
+    -> Int
+computeGutterWidth (CompoundTable sections) =
+    sections # map (\(TableSection { rows }) ->
+        if Array.null rows then 0
+        else if Array.all (_.sub >>> Array.null) rows
+        then 1 else 2
+    ) # maximum # fromMaybe 0
+
+
+mcons :: forall a. Maybe a -> Array a -> Array a
+mcons (Just head) tail = Array.cons head tail
+mcons Nothing tail = tail
+
+instance displayTable :: (
+    Display sectionT, Display majRT, Display minRT, Display majCT, Display minCT, Display dataT
+) => Display (CompoundTable sectionT majRT minRT majCT minCT dataT) where
+    display ctable@(CompoundTable [TableSection section]) =
+        HH.table_><HH.tbody_ $ map HH.tr_ rows
+        where
+            gutterWidth = computeGutterWidth ctable
+            cell a
+                | gutterWidth == 0 = Nothing
+                | otherwise =
+                    Just $ HH.th [ HP.colSpan gutterWidth ] a
+            mkMajRowH contents
+                | gutterWidth == 0 =
+                    HH.th_ >< contents
+                | otherwise =
+                    HH.th [ HP.colSpan gutterWidth ] >< contents
+            labelCellM = cell [ display section.section ]
+            padCellM = cell $ []
+            headerMajCols =
+                section.cols # map (\{ label, sub } ->
+                    HH.th [ HP.colSpan $ max 1 $ Array.length sub ] >< display label
+                )
+            header = mcons labelCellM headerMajCols
+            subcol2s =
+                section.cols # Array.concatMap (\{ label, sub } ->
+                    map (Tuple label) sub
+                )
+            continue majRow minRow =
+                subcol2s # map (\(Tuple majCol minCol) ->
+                    HH.td_ >< display $ section.getCell section.section majRow minRow majCol minCol
+                )
+            subheaderM =
+                if Array.any (\{ sub } -> not $ Array.null sub) section.cols
+                then
+                    section.cols # Array.concatMap (\{ sub } ->
+                        case sub of
+                            [] ->
+                                [ HH.th_ [] ]
+                            _ ->
+                                map (\h -> HH.th_ >< display h) sub
+                    ) # Just
+                else Nothing
+            headerrows =
+                case subheaderM of
+                    Nothing -> [ header ]
+                    Just subheader ->
+                        [ header, mcons padCellM subheader ]
+            simplerows =
+                Array.all (\{ sub } -> Array.null sub) section.cols
+            contentrows =
+                section.rows # Array.concatMap mkrow
+            mkrow { label, sub } =
+                if simplerows
+                then
+                    [ mcons padCellM [ HH.th_ [ display label ] ] ]
+                else
+                    let
+                        subrows = map (\sublabel -> [ HH.th_ [], HH.th_ [ display sublabel ] ] <> continue label sublabel) sub
+                    in [ [ mkMajRowH $ display label ] ] <> subrows
+            rows = headerrows <> contentrows
+    display _ = display "table"
+
+{-
+computeCellWidths :: forall majT minT. Header majT minT -> Header (majT, Int) (minT, Int)
+
+let
+    labelCell = case computeGutterWidth table of
+        0 -> Nothing
+        1 -> Just $ H.th_ $ display section.section
+        width -> Just $ H.th [ HA.colspan width ] $ display section.section
+    header =
+
+in mcons labelCell header
+-}
+
 nomen :: Noun
 nomen =
     { rowLabels: "nom" +> "gen" +> "dat" +> "acc" +> "abl" +> empty
@@ -60,6 +234,84 @@ nomen =
         empty
     }
 
+data Degree = Positive | Comparative | Superlative
+instance showDegree :: Show Degree where
+    show Positive = "positive"
+    show Comparative = "comparative"
+    show Superlative = "superlative"
+instance displayDegree :: Display Degree where
+    display = display <<< show
+
+data Person = FirstP | SecondP | ThirdP
+instance showPerson :: Show Person where
+    show FirstP = "1st person"
+    show SecondP = "2nd person"
+    show ThirdP = "3rd person"
+instance displayPerson :: Display Person where
+    display = display <<< show
+
+data Number2 = Singular | Plural
+instance showNumber2 :: Show Number2 where
+    show Singular = "singular"
+    show Plural = "plural"
+instance displayNumber2 :: Display Number2 where
+    display = display <<< show
+
+data Tense = Present | Imperfect | Future | Perfect | Pluperfect | FuturePerfect
+instance showTense :: Show Tense where
+    show Present = "present"
+    show Imperfect = "imperfect"
+    show Future = "future"
+    show Perfect = "perfect"
+    show Pluperfect = "pluperfect"
+    show FuturePerfect = "future-perfect"
+instance displayTense :: Display Tense where
+    display = display <<< show
+
+data Voice = Active | Passive
+instance showVoice :: Show Voice where
+    show Active = "active"
+    show Passive = "passive"
+instance displayVoice :: Display Voice where
+    display = display <<< show
+
+data Mood = Indicative | Subjunctive
+instance showMood :: Show Mood where
+    show Indicative = "indicative"
+    show Subjunctive = "subjunctive"
+instance displayMood :: Display Mood where
+    display = display <<< show
+
+celeriter :: CompoundTable Unit Degree Void Void Void String
+celeriter = simpleVertical
+    { rows: [Positive, Comparative, Superlative]
+    , getCell: case _ of
+        Positive -> "celeriter"
+        Comparative -> "celerius"
+        Superlative -> "celerrimē"
+    }
+
+headerproduct :: forall majT minT. Array majT -> Array minT -> Headers majT minT
+headerproduct major minor =
+    map (\label -> { label, sub: minor }) major
+
+getCellVolo :: Mood -> Voice -> Tense -> Number2 -> Person -> String
+getCellVolo Indicative Active Present Singular FirstP = "volo"
+getCellVolo Indicative Active Present Singular SecondP = "vis"
+getCellVolo Indicative Active Present Singular ThirdP = "volunt"
+getCellVolo Indicative Active Present Plural FirstP = "volumus"
+getCellVolo Indicative Active Present Plural SecondP = "vultis"
+getCellVolo Indicative Active Present Plural ThirdP = "volunt"
+getCellVolo _ _ _ _ _ = "UNK"
+
+volo :: CompoundTable Mood Voice Tense Number2 Person String
+volo = CompoundTable ([TableSection
+    { section: Indicative
+    , rows: headerproduct [Active] [Present]
+    , cols: headerproduct [Singular, Plural] [FirstP, SecondP, ThirdP]
+    , getCell: getCellVolo
+    }])
+
 type State =
     { on :: Boolean
     , text :: String
@@ -71,44 +323,10 @@ initialState =
     , text: "Hello"
     }
 
-button :: forall a b. (MouseEvent -> Maybe b) -> Array HH.ClassName -> String -> HH.HTML a b
-button cmd classes label =
-  HH.button
-    [ HE.onClick cmd
-    , HP.classes $
-        append classes
-          [ MDL.button
-          , MDL.jsButton
-          , MDL.jsRippleEffect
-          , Button._raised
-          ]
-    ]
-    [ HH.text label ]
-
-input :: forall a b. (Event -> Maybe b) -> Array HH.ClassName -> String -> String -> HH.HTML a b
-input cmd classes label value =
-    mdiv [ MDL.textfield, MDL.jsTextfield ]
-        [ HH.input
-            [ HP.classes [ Textfield.input ]
-            , HP.type_ InputText
-            , HP.id_ "sample1"
-            , HE.onInput cmd
-            , HP.value value
-            ]
-        , HH.label
-            [ HP.classes [ Textfield.label ]
-            , HP.for "sample1"
-            ]
-            [ HH.text label ]
-        ]
-
-mdiv :: forall a b. Array HH.ClassName -> Array (HH.HTML a b) -> HH.HTML a b
-mdiv classes children =
-    HH.div [ HP.classes classes ] children
-mdiv1 :: forall a b. HH.ClassName -> Array (HH.HTML a b) -> HH.HTML a b
-mdiv1 = mdiv <<< Array.singleton
-
-table :: forall cell nRows nCols a b. (Nat nRows, Nat nCols) => Inflection nRows nCols cell -> (cell -> HH.HTML a b) -> HH.HTML a b
+table :: forall cell nRows nCols a b. (Nat nRows, Nat nCols)
+    => Inflection nRows nCols cell
+    -> (cell -> HH.HTML a b)
+    -> HH.HTML a b
 table datable mapper = HH.table_><HH.tbody_ (map HH.tr_ (Array.cons header rows))
     where
       header = Array.cons (HH.th_/> "") $ map (HH.th[style (textAlign leftTextAlign)]/> _) $ toArray datable.colLabels
@@ -127,8 +345,10 @@ ui = H.component { render, eval, initialState: const initialState, receiver: con
   render state =
     mdiv1 Layout.container
     ><mdiv [ MDL.layout ]
-      ><mdiv1 Layout.content
+      [ mdiv1 Layout.content
         ><table nomen (input (HE.input UserInput) [] "Form of nomen" >>> (HH.td_><_))
+      , mdiv1 Layout.content>< display volo
+      ]
 
   eval :: Query ~> H.ComponentDSL State Query Void (Aff (dom :: DOM | eff))
   eval (ToggleState next) = do
